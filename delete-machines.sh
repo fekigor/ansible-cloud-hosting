2e97edaeb59dedc5a54289c1ef4eb6aa98c84269
Igor Feklin <igor.feklin@gmail.com>
2017-03-06 13:19:06 +0000

    Added code to unset variables. Changes in echo commands with escape codes

--- delete-machines.sh
+++ delete-machines.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 machines=$(docker-machine ls |grep -v NAME|awk '{print $1}')
-[ ! -z "${machines}" ] &&  docker-machine rm -y  ${machines}
-[ -z "${machines}" ] && echo -e "\033[1mNothing to do!\033[0m"
+if [ ! -z "${machines}" ]; then
+	docker-machine rm -y  ${machines}
+	if [ -s exportfile ]; then
+		cat exportfile|cut -f 1 -d"="|sed -e 's/^export/unset/g' > unsetfile
+		echo -e "\033[1m# Run this command to configure your shell: \033[0m"
+		echo "# eval \$(cat ./unsetfile)"
+	fi 
+fi
+if [ -z "${machines}" ]; then  
+	echo -e "\033[1mNothing to do!\033[0m"
+fi
 exit 0
--- prepare-access-2-machines.sh
+++ prepare-access-2-machines.sh
@@ -1,61 +1,74 @@
 #!/bin/bash
 export PUBLIC_KEY="/root/.ssh/id_rsa.pub"
 
-echo "Unset server1_ip variable ${server1_ip} ..."
+echo -e "\033[1mRemoves known_hosts ...\033[0m"
 	ssh-keygen -f "/root/.ssh/known_hosts" -R "${server1_ip}"
-unset server1_ip
 sleep 1
 echo "Unset server2_ip variable ${server2_ip} ..."
 	ssh-keygen -f "/root/.ssh/known_hosts" -R "${server2_ip}"
-unset server2_ip
 sleep 1
 echo "Unset server3_ip variable ${server3_ip} ..."
-	ssh-keygen -f "/root/.ssh/known_hosts" -R ${server3_ip}"
-unset server3_ip
-echo "Unsets done"
+	ssh-keygen -f "/root/.ssh/known_hosts" -R "${server3_ip}"
+echo -e "\033[1mRemoves done\033[1m"
 
 
-echo "Unset server1_cert variable ..."
-unset $(docker-machine env server1 |grep NAME|cut -f 2 -d"="|tr -d \")_cert
-echo "Unset server2_cert variable ..."
-unset $(docker-machine env server2 |grep NAME|cut -f 2 -d"="|tr -d \")_cert
-echo "Unset server3_cert variable ..."
-unset $(docker-machine env server3 |grep NAME|cut -f 2 -d"="|tr -d \")_cert
-echo "Unsets done"
+#echo "Unset server1_cert variable ..."
+#unset $(docker-machine env server1 |grep NAME|cut -f 2 -d"="|tr -d \")_cert
+#echo "Unset server2_cert variable ..."
+#unset $(docker-machine env server2 |grep NAME|cut -f 2 -d"="|tr -d \")_cert
+#echo "Unset server3_cert variable ..."
+#unset $(docker-machine env server3 |grep NAME|cut -f 2 -d"="|tr -d \")_cert
+
+if [ -s exportfile ]; then
+	cat exportfile|cut -f 1 -d"="|sed -e 's/^export/unset/g' > unsetfile
+	[ -s unsetfile ] && eval $(cat ./unsetfile)
+else
+	echo -e "\033[1mUnset variables ...\033[0m"
+	echo "Unset variables: server1_ip server1_cert ..."
+	unset server1_ip server1_cert
+	echo "Unset variables: server1_ip server1_cert ..."
+	unset server2_ip server2_cert
+	echo "Unset variables: server1_ip server1_cert ..."
+	unset server3_ip server3_cert
+	
+fi 
 
 
-echo "Sets new values for server1..."
+echo -e "\033[1mUnsets done\033[0m"
+
+
+echo -e "\033[1mSets new values for server1...\033[0m"
 export $(docker-machine env server1 |grep NAME|cut -f 2 -d"="|tr -d \")_ip=$(docker-machine env server1 |grep HOST|cut -f 3 -d"/"|cut -f 1 -d":")
 export $(docker-machine env server1 |grep NAME|cut -f 2 -d"="|tr -d \")_cert=$(docker-machine env server1 |grep CERT|cut -f 2 -d"="|tr -d \")/id_rsa
 echo "Server1: IP ${server1_ip} || CERT - ${server1_cert}"
-echo "Sets new values for server2..."
+echo -e "\033[1mSets new values for server2...\033[0m"
 export $(docker-machine env server2 |grep NAME|cut -f 2 -d"="|tr -d \")_ip=$(docker-machine env server2 |grep HOST|cut -f 3 -d"/"|cut -f 1 -d":")
 export $(docker-machine env server2 |grep NAME|cut -f 2 -d"="|tr -d \")_cert=$(docker-machine env server2 |grep CERT|cut -f 2 -d"="|tr -d \")/id_rsa
 echo "Server2: IP ${server2_ip} || CERT - ${server2_cert}"
-echo "Sets new values for server3..."
+echo -e "\033[1mSets new values for server3...\033[0m"
 export $(docker-machine env server3 |grep NAME|cut -f 2 -d"="|tr -d \")_ip=$(docker-machine env server3 |grep HOST|cut -f 3 -d"/"|cut -f 1 -d":")
 export $(docker-machine env server3 |grep NAME|cut -f 2 -d"="|tr -d \")_cert=$(docker-machine env server3 |grep CERT|cut -f 2 -d"="|tr -d \")/id_rsa
 echo "Server3: IP ${server3_ip} || CERT - ${server3_cert}"
-echo "Sets DONE"
+echo -e "\033[1mSets DONE\033[0m"
 
 
-echo "Copying id_rsa.pub to server1 ..."
+echo -e "\033[1mCopying\033[0m id_rsa.pub to server1 ..."
 echo -e "\033[1m At this step you may input YES to continue ...\033[0m"
 scp -i ${server1_cert} ${PUBLIC_KEY} root@${server1_ip}:/root/
 echo "Adding id_rsa.pub to server1 /root/.ssh/authorized_keys ..."
 ssh -i ${server1_cert} root@${server1_ip} 'PUB=$(cat /root/id_rsa.pub|cut -f 3 -d" "); CHECK=$(grep -n ${PUB} /root/.ssh/authorized_keys); [ -z "${CHECK}" ] && ecbo "${PUB}" >> /root/.ssh/authorized_keys;rm /root/id_rsa.pub'
-echo "Copying id_rsa.pub to server2 ..."
+echo -e "\033[1mCopying\033[0m id_rsa.pub to server2 ..."
 echo -e "\033[1m At this step you may input YES to continue ...\033[0m"
 scp -i ${server2_cert} ${PUBLIC_KEY} root@${server2_ip}:/root/
 echo "Adding id_rsa.pub to server2 /root/.ssh/authorized_keys ..."
 ssh -i ${server2_cert} root@${server2_ip} 'PUB=$(cat /root/id_rsa.pub|cut -f 3 -d" "); CHECK=$(grep -n ${PUB} /root/.ssh/authorized_keys); [ -z "${CHECK}" ] && ecbo "${PUB}" >> /root/.ssh/authorized_keys;rm /root/id_rsa.pub'
-echo "Copying id_rsa.pub to server3 ..."
+echo -e "\033[1mCopying\033[0m id_rsa.pub to server3 ..."
 echo -e "\033[1m At this step you may input YES to continue ...\033[0m"
 scp -i ${server3_cert} ${PUBLIC_KEY} root@${server3_ip}:/root/
 echo "Adding id_rsa.pub to server3 /root/.ssh/authorized_keys ..."
 ssh -i ${server3_cert} root@${server3_ip} 'PUB=$(cat /root/id_rsa.pub|cut -f 3 -d" "); CHECK=$(grep -n ${PUB} /root/.ssh/authorized_keys); [ -z "${CHECK}" ] && ecbo "${PUB}" >> /root/.ssh/authorized_keys;rm /root/id_rsa.pub'
 
-echo "Prepare access DONE"
+echo -e "Prepare access \033[1mDONE\033[0m"
 
 echo "export server1_ip=${server1_ip}" > exportfile
 echo "export server1_cert=${server1_cert}" >> exportfile
@@ -63,7 +76,7 @@ echo "export server2_ip=${server2_ip}" >> exportfile
 echo "export server2_cert=${server2_cert}" >> exportfile
 echo "export server3_ip=${server3_ip}" >> exportfile
 echo "export server3_cert=${server3_cert}" >> exportfile
-echo -e "\033[1m # Run this command to configure your shell: \033[0m"
-echo "# eval \$(cat ./exportfile)"
+echo -e "\033[1m# Run this command to configure your shell: \033[0m"
+echo -e "\033[1m# eval \$(cat ./exportfile)\033[0m"
 
 exit 0
